1646bd36ce854ff192d939e7e68330d8a584bb39
Byungkook Oh <bkoh509@gmail.com>
2024-05-19 16:30:21 +0900

    add blank pages

---
+++ src/app/board/teaching/page.tsx
@@ -0,0 +1,25 @@
+import {Metadata} from "next";
+import NotFound from "@/app/not-found";
+import {getMetadata} from "@/lib/GetMetadata";
+import {SubCover} from "@/components/Covers";
+
+const TITLE = `Teaching`
+
+export const generateMetadata = async (): Promise<Metadata> => {
+    return getMetadata({
+        title: TITLE,
+    });
+};
+
+export default function Page() {
+    return (
+        <>
+            <div className="max-w-screen-2xl mx-auto">
+                <SubCover title={TITLE}/>
+            </div>
+            <div className="max-w-screen-xl mx-auto">
+                <NotFound/>
+            </div>
+        </>
+    )
+}
