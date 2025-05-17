6a59b8f8b5c0074bcd63cc8cac191b0325f96f4c
GLI Lab <glilab509@google.com>
2025-05-17 14:58:31 +0900

    refactor news section and add KakaoMap component; replace static news data with dynamic NewsList component, and enhance contact page with email details and location map

--- src/app/board/news/page.tsx
+++ src/app/board/news/page.tsx
@@ -1,7 +1,7 @@
 import {Metadata} from "next";
 import {getMetadata} from "@/lib/GetMetadata";
 import {SubCover} from "@/components/Covers";
-import {newsData} from "@/assets/data/news";
+import {NewsList} from "@/components/News";
 
 const TITLE = `News`
 
@@ -19,18 +19,15 @@ export default function Page() {
             </div>
 
             <div className="max-w-screen-2xl mx-auto bg-white">
-                <div className="max-w-screen-xl mx-auto flex flex-col items-center justify-center text-center space-y-8
+                <div className="max-w-screen-xl mx-auto flex flex-col items-center text-center space-y-8
                                 py-8 md:py-16 px-4 md:px-6">
                     <p className="font-bold tracking-tighter text-[28px] md:text-[36px]">
                         Latest News
                     </p>
-                    <div className="flex flex-col text-home items-start text-left space-y-2">
-                        {newsData.map(({id, date, content}) => (
-                            <p key={id}>
-                                <span className="font-semibold">[{date}]</span> {content}
-                            </p>
-                        ))}
-                    </div>
+                    <NewsList 
+                        className="w-full text-home text-left"
+                        count={30}
+                    />
                 </div>
             </div>
 
