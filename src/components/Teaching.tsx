13d2fb995143b898422e976e5489e724ebb65f9b
GLI Lab <glilab509@gmail.com>
2025-05-18 15:43:11 +0900

    update API URL in .env and .env.development files

--- src/components/Teaching.tsx
+++ src/components/Teaching.tsx
@@ -1,39 +1,39 @@
-import teachingData from '@/assets/data/teaching.json';
+// import teachingData from '@/assets/data/teaching.json';
 
-export function TeachingList({ className = '', count = teachingData.length }) {
-    const latestTeaching = [...teachingData]
-        .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
-        .slice(0, count);
+// export function TeachingList({ className = '', count = teachingData.length }) {
+//     const latestTeaching = [...teachingData]
+//         .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+//         .slice(0, count);
 
-    return (
-        <div className={className}>
-            <div className="grid grid-cols-[auto,1fr] gap-x-2 gap-y-2">
-                {latestTeaching.map((teaching) => {
-                    const formattedDate = new Date(teaching.date).toLocaleDateString('en-US', {
-                        year: 'numeric',
-                        month: 'short'
-                    });
+//     return (
+//         <div className={className}>
+//             <div className="grid grid-cols-[auto,1fr] gap-x-2 gap-y-2">
+//                 {latestTeaching.map((teaching) => {
+//                     const formattedDate = new Date(teaching.date).toLocaleDateString('en-US', {
+//                         year: 'numeric',
+//                         month: 'short'
+//                     });
 
-                    const [title, ...description] = teaching.content.split('\n');
-                    const descriptionText = description.length > 0 ? ` - ${description.join(' ')}` : '';
+//                     const [title, ...description] = teaching.content.split('\n');
+//                     const descriptionText = description.length > 0 ? ` - ${description.join(' ')}` : '';
 
-                    return (
-                        <div key={teaching.date} className="contents">
-                            <div className="font-semibold">
-                                [{formattedDate}]
-                            </div>
-                            <div>
-                                {title}
-                                {descriptionText && (
-                                    <span className="italic">
-                                        {descriptionText}
-                                    </span>
-                                )}
-                            </div>
-                        </div>
-                    );
-                })}
-            </div>
-        </div>
-    );
-} 
+//                     return (
+//                         <div key={teaching.date} className="contents">
+//                             <div className="font-semibold">
+//                                 [{formattedDate}]
+//                             </div>
+//                             <div>
+//                                 {title}
+//                                 {descriptionText && (
+//                                     <span className="italic">
+//                                         {descriptionText}
+//                                     </span>
+//                                 )}
+//                             </div>
+//                         </div>
+//                     );
+//                 })}
+//             </div>
+//         </div>
+//     );
+// } 
