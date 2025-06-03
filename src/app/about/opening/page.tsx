3b72dafc1f1d8fe04e2c5f75caf632afbdb6c6f8
GLI Lab <glilab509@gmail.com>
2025-06-03 12:52:15 +0900

    Update contact information

--- src/app/about/opening/page.tsx
+++ src/app/about/opening/page.tsx
@@ -1,7 +1,7 @@
 import {Metadata} from "next";
-import NotFound from "@/app/not-found";
 import {getMetadata} from "@/lib/GetMetadata";
 import {SubCover} from "@/components/Covers";
+import Link from "next/link";
 
 const TITLE = `Opening`
 
@@ -29,11 +29,19 @@ export default function Page() {
                     </p>
 
                     <p className="text-home">We are currently seeking <span className="highlight">talented and passionate students (MS/PhD) as well as undergraduate research interns</span>.
+                        <br /> 
                         Please feel free to contact us <span
                             className="font-semibold">bkoh@konkuk.ac.kr (오병국 교수)</span> if you are interested in our
                         research.</p>
 
-                    <p className="text-home">LLM, 추천시스템, 그래프 머신러닝과 지식 기반의 지능형 시스템에 관심이 있는 대학원 지망생과 학부연구생은 언제든지 연락 주시기 바랍니다.</p>
+                    <p className="text-home">LLM, 추천시스템, 그래프 머신러닝과 지식 기반의 지능형 시스템에 관심이 있는 대학원 지망생과 학부연구생은 언제든지 연락 주시기 바랍니다. 
+                        <br />
+                        선별된 핵심 논문 <Link href="/board/study" className="group text-brand-primary underline-offset-4 hover:underline hover:decoration-1">
+                        스터디
+                            <svg className="w-[0.66em] h-[0.66em] ml-0.5 inline opacity-60 group-hover:opacity-100" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+                            </svg>
+                        </Link>를 통해 충분히 성장가능합니다.</p>
                     
                     <div className="text-home flex flex-col text-left mb-6">
                         <p className="font-semibold mb-2">
@@ -42,7 +50,7 @@ export default function Page() {
                         <ul className="list-inside list-disc space-y-2">
                             <li>정기적인 논문 스터디 및 우수논문 작성/참여 기회 제공</li>
                             <li>학술적 성과에 따른 추가 장려금 지원</li>
-                            <li>논문 게제료 및 국내외 학술대회 참석 지원</li>
+                            <li>논문 게재료 및 국내외 학술대회 참석 지원</li>
                             <li>연구 장비 및 소프트웨어 사용 지원</li>
                         </ul>
                     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -119,12 +119,14 @@ export default function Page() {
                     </p>
 
                     <p className="text-home">We are currently seeking <span className="highlight">talented and passionate students (MS/PhD) as well as undergraduate research interns</span>.
+                        <br /> 
                         Please feel free to contact us <span
                             className="font-semibold">bkoh@konkuk.ac.kr (오병국 교수)</span> if you are interested in our
                         research.</p>
 
-                    <p className="text-home">LLM, 추천시스템, 그래프 머신러닝과 지식 기반의 지능형 시스템에 관심이 있는 대학원 지망생과 학부연구생은 언제든지 연락 주시기 바랍니다. 아직 딥러닝에 익숙하지 않아도 성실함만 있으면 충분합니다. 
-                        <Link href="/board/study" className="group text-brand-primary underline-offset-4 hover:underline hover:decoration-1">
+                    <p className="text-home">LLM, 추천시스템, 그래프 머신러닝과 지식 기반의 지능형 시스템에 관심이 있는 대학원 지망생과 학부연구생은 언제든지 연락 주시기 바랍니다. 
+                        <br />
+                        선별된 핵심 논문 <Link href="/board/study" className="group text-brand-primary underline-offset-4 hover:underline hover:decoration-1">
                         스터디
                             <svg className="w-[0.66em] h-[0.66em] ml-0.5 inline opacity-60 group-hover:opacity-100" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
